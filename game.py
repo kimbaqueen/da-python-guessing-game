92d0ec465396460afaa5b41ec74f091ea4a53e49
kimbaqueen <k.a.starr@live.com>
2022-07-30 15:44:43 -0600

    completed exercise

--- game.py
+++ game.py
@@ -15,28 +15,38 @@
 #     else:
 #         congratulate player
 
+from random import randint
+
+
+tries = 0
+number = randint(1, 100)
+
 print("Hello! What's your name?")
+
 name = input("Type your name here: ")
 
+print(f"{name}, I'm thinking of a number between 1 and 100. \nCan you guess what the number is?")
 
+while True:
+    guess = input("Enter your number guess: ")
 
-# Jessica, I'm thinking of a number between 1 and 100.
-# Try to guess my number.
-# Your guess? 50
-# Your guess is too low, try again.
-# Your guess? 80
-# Your guess is too high, try again.
-# Your guess? 60
-# Your guess is too low, try again.
-# Your guess? 70
-# Your guess is too high, try again.
-# Your guess? 63
-# Your guess is too low, try again.
-# Your guess? 64
-# Your guess is too low, try again.
-# Your guess? 67
-# Your guess is too low, try again.
-# Your guess? 69
-# Your guess is too high, try again.
-# Your guess? 68
-# Well done, Jessica! You found my number in 9 tries!
+    try:
+        guess = int(guess)
+    except ValueError:
+        print(f"{guess} is not a valid number, try again.")
+        continue
+
+    if guess < 1 and guess > 100:
+        print("Please guess a number between 1 and 100. Try again.")
+        continue
+    
+    tries += 1
+
+    if guess < number:
+        print("Too low, try again.")
+    elif guess > number:
+        print("Too high, try again")
+    else:
+        print(f"Congrats {name}! \nYou guessed my number in {tries} attempts!")
+        break
+
