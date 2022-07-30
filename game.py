b17c376ea8d8792adab2a6655492817a97de7fa9
kimbaqueen <k.a.starr@live.com>
2022-07-30 15:20:24 -0600

    added greeting and name input

---
+++ game.py
@@ -0,0 +1,42 @@
+"""A number-guessing game."""
+
+# Put your code here
+# print("hello!")
+
+# psuedocode:
+# greet player
+# get player name
+# choose random number between 1 and 100
+# repeat forever:
+#     get guess
+#     if guess is incorrect:
+#         give hint
+#         increase number of guesses
+#     else:
+#         congratulate player
+
+print("Hello! What's your name?")
+name = input("Type your name here: ")
+
+
+
+# Jessica, I'm thinking of a number between 1 and 100.
+# Try to guess my number.
+# Your guess? 50
+# Your guess is too low, try again.
+# Your guess? 80
+# Your guess is too high, try again.
+# Your guess? 60
+# Your guess is too low, try again.
+# Your guess? 70
+# Your guess is too high, try again.
+# Your guess? 63
+# Your guess is too low, try again.
+# Your guess? 64
+# Your guess is too low, try again.
+# Your guess? 67
+# Your guess is too low, try again.
+# Your guess? 69
+# Your guess is too high, try again.
+# Your guess? 68
+# Well done, Jessica! You found my number in 9 tries!
